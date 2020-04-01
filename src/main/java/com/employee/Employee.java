b431214e1cca95d18f32135a64b5d8933206464a
sayalipawade <sayalipawade05@gmail.com>
2020-04-01 13:05:42 +0530

    Added:Checking employee is present or absent

--- src/main/java/com/employee/Employee.java
+++ src/main/java/com/employee/Employee.java
@@ -1,9 +1,28 @@
 package com.employee;
 
+import java.util.Random;
+
 public class Employee
 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation");
+    //Checking employee is present or absent
+    public static void attendenceChecking(int IS_EMP_PRESENT)
+    {
+        Random randNo=new Random();
+        int empCheck=randNo.nextInt()%2;
+        if(empCheck==1)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
     }
-
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to employee wage computation");
+        //Constants
+        int IS_EMP_PRESENT=1;
+        attendenceChecking(IS_EMP_PRESENT);
+    }
 }
