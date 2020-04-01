d25947beb419e99ff8f42ca4c51cb79e592f5d78
sayalipawade <sayalipawade05@gmail.com>
2020-04-01 17:46:48 +0530

    Added:Calculating wages till condition of total working hours and days

--- src/main/java/com/employee/Employee.java
+++ src/main/java/com/employee/Employee.java
@@ -7,15 +7,18 @@ public class Employee
     public static final int IS_EMP_PART_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int WORKING_DAYS_IN_MONTH=20;
+    public static final int MAXIMUM_WORK_HOURS=100;
     //Calculating monthly wage of employee
-    public static void calculateDailyWage()
+    public static void calculateWage()
     {
         //variables
         int empHours=0;
         int empDailyWage=0;
         int empMonthlyWage=0;
-
-        for(int i=1;i<=WORKING_DAYS_IN_MONTH;i++)
+        int totalEmpHours=0;
+        int totalSalary=0;
+        int totalWorkingDays=0;
+        while(totalEmpHours<MAXIMUM_WORK_HOURS && totalWorkingDays<WORKING_DAYS_IN_MONTH)
         {
             Random randNo = new Random();
             int empCheck = randNo.nextInt() % 3;
@@ -30,14 +33,15 @@ public class Employee
                 default:
                             empHours = 0;
             }
-            empDailyWage = empHours * EMP_RATE_PER_HOUR;
-            empMonthlyWage=empMonthlyWage+empDailyWage;
+            totalEmpHours=totalEmpHours+empHours;
+            totalWorkingDays++;
         }
-        System.out.println("Employee monthly wage="+empMonthlyWage);
+        totalSalary=totalEmpHours*EMP_RATE_PER_HOUR;
+        System.out.println("Total Salary of employee="+totalSalary);
     }
     public static void main(String[] args)
     {
         System.out.println("Welcome to employee wage computation");
-        calculateDailyWage();
+        calculateWage();
     }
 }
