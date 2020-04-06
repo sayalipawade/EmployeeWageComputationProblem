0d72bfb8b4af4e0e6c103be79eb74c8cfd436577
sayalipawade <sayalipawade05@gmail.com>
2020-04-07 00:09:25 +0530

    Refactored:Use some object oriented concepts

--- src/main/java/com/employee/Employee.java
+++ src/main/java/com/employee/Employee.java
@@ -3,27 +3,34 @@ import java.util.Random;
 public class Employee
 {
     //Constants
-    public static final int IS_EMP_FULL_TIME=1;
-    public static final int IS_EMP_PART_TIME=2;
-    public static final int EMP_RATE_PER_HOUR=20;
-    public static final int WORKING_DAYS_IN_MONTH=20;
-    public static final int MAXIMUM_WORK_HOURS=100;
+    public static final int IS_EMP_FULL_TIME = 1;
+    public static final int IS_EMP_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int WORKING_DAYS_IN_MONTH = 20;
+    public static final int MAXIMUM_WORK_HOURS = 100;
+    public static final int FULL_TIME_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
+
+    //variables
+    public static int totalEmpHours = 0;
+    public static int totalSalary = 0;
+    public static int totalWorkingDays = 0;
+    public static int empHours = 0;
+    public static int DailyWage = 0;
 
     //getting employee working hours
     public static int getWorkingHours(int empCheck)
     {
-        //variables
-        int empHours = 0;
         switch (empCheck)
         {
             case IS_EMP_FULL_TIME:
-                empHours = 8;
-                break;
+                    empHours = FULL_TIME_HOURS;
+                    break;
             case IS_EMP_PART_TIME:
-                empHours = 4;
-                break;
+                    empHours = PART_TIME_HOURS;
+                    break;
             default:
-                empHours = 0;
+                    empHours = 0;
         }
         return empHours;
     }
@@ -31,36 +38,31 @@ public class Employee
     //Calculating daily wage of employee
     public static int calculateDailyWage(int empHours)
     {
-        int DailyWage=0;
-        DailyWage=empHours*EMP_RATE_PER_HOUR;
+        DailyWage = empHours * EMP_RATE_PER_HOUR;
         return DailyWage;
     }
 
     //calculating monthly wage of employee
     public static void calculateMonthlyWage()
     {
-        //variables
-        int totalEmpHours = 0;
-        int totalSalary = 0;
-        int totalWorkingDays = 0;
-        int empHours=0;
-        int empDailyWage[]=new int[100];
+        int empDailyWage[] = new int[100];
         while (totalEmpHours < MAXIMUM_WORK_HOURS && totalWorkingDays < WORKING_DAYS_IN_MONTH)
         {
             Random randNo = new Random();
-            int empCheck = randNo.nextInt()%3;
+            int empCheck = randNo.nextInt() % 3;
             empHours = getWorkingHours(empCheck);
             totalEmpHours = totalEmpHours + empHours;
             totalWorkingDays++;
-            empDailyWage[totalWorkingDays]=calculateDailyWage(empHours);
-            System.out.println(totalWorkingDays+"="+empDailyWage[totalWorkingDays]);
-            totalSalary=totalSalary+empDailyWage[totalWorkingDays];
+            empDailyWage[totalWorkingDays] = calculateDailyWage(empHours);
+            System.out.println(totalWorkingDays + "=" + empDailyWage[totalWorkingDays]);
+            totalSalary = totalSalary + empDailyWage[totalWorkingDays];
         }
-        System.out.println("Total salary of employee="+totalSalary);
+        System.out.println("Total salary of employee=" + totalSalary);
     }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to employee wage computation");
-        calculateMonthlyWage();
+            System.out.println("Welcome to employee wage computation");
+            Employee employee=new Employee();
+            employee.calculateMonthlyWage();
     }
 }
